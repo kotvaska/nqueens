25da28649d24dbbcf73236a3f22722eba857a379
kotvaska <zolotykh24@gmail.com>
2018-01-29 21:17:49 +0500

    Changed to binary coding

--- nqueens.py
+++ nqueens.py
@@ -4,6 +4,8 @@ import copy
 import math
 import random
 
+debug = False
+
 
 class Board:
     def __init__(self, fitness, board_size=8, mut_prob=1.0):
@@ -13,13 +15,11 @@ class Board:
 
         self.fitness = 0
 
-        self.queens = list(range(self.board_size))
+        self.queens = ["{0:03b}".format(i) for i in range(self.board_size)]
         self.move_gen(self.board_size / 2)
 
     def move_gen(self, count):
-        count = int(count)
-
-        for i in range(count):
+        for i in range(int(count)):
             j = random.randint(0, self.board_size - 1)
             k = random.randint(0, self.board_size - 1)
             self.queens[j], self.queens[k] = self.queens[k], self.queens[j]
@@ -37,13 +37,13 @@ class Board:
 
         for i in range(self.board_size):
             for j in range(i + 1, self.board_size):
-                if math.fabs(self.queens[i] - self.queens[j]) == j - i:
+                if math.fabs(int(self.queens[i], 2) - int(self.queens[j], 2)) == j - i:
                     self.fitness -= 1
 
     def visualization(self):
         board = ""
         for row in range(self.board_size):
-            queen = self.queens.index(row)
+            queen = self.queens.index("{0:03b}".format(row))
 
             for col in range(self.board_size):
                 if col == queen:
@@ -65,7 +65,7 @@ class Solver_8_queens:
         self.mut_prob = mut_prob
 
         self.population = []
-        self.generation_count = 0
+        self.__generation_count = 0
 
     def __found_match(self):
         for population in self.population:
@@ -74,10 +74,8 @@ class Solver_8_queens:
         return False
 
     def __random_selection(self):
-        population_list = []
-        for i in range(len(self.population)):
-            population_list.append((i, self.population[i].fitness))
-        population_list.sort(key=lambda pop_item: pop_item[1], reverse=True)
+        population_list = [(i, item.fitness) for i, item in enumerate(self.population)]
+        population_list.sort(key=lambda item: item[1], reverse=True)
         return population_list[:int(len(population_list) / self.cross_prob)]
 
     def __first_generation(self):
@@ -87,12 +85,13 @@ class Solver_8_queens:
         self.__print_population()
 
     def __next_generation(self):
-        self.generation_count += 1
+        self.__generation_count += 1
         selections = self.__random_selection()
         new_population = []
 
         while len(new_population) < self.population_size:
-            sel = random.choice(selections)[0]
+            sel = random.choice(selections)[0] if random.random() < self.cross_prob \
+                else random.randint(0, self.population_size - 1)
             new_population.append(copy.deepcopy(self.population[sel]))
         self.population = new_population
 
@@ -102,7 +101,10 @@ class Solver_8_queens:
         self.__print_population(selections)
 
     def __print_population(self, selections=None):
-        print("Population #%d" % self.generation_count)
+        if debug is False:
+            return
+
+        print("Population #%d" % self.__generation_count)
 
         if selections is None:
             selections = []
@@ -122,25 +124,25 @@ class Solver_8_queens:
         self.generation_size = self.generation_size if max_epochs is None else max_epochs
         self.fitness = self.fitness if min_fitness is None else min_fitness
         self.population = []
-        self.generation_count = 0
+        self.__generation_count = 0
 
         self.__first_generation()
 
         while True:
             if self.__found_match() is True:
                 break
-            if -1 < self.generation_size <= self.generation_count:
+            if -1 < self.generation_size <= self.__generation_count:
                 break
 
             self.__next_generation()
 
-        if -1 < self.generation_size <= self.generation_count:
-            print("Couldn't find result in %d generations" % self.generation_count)
+        if -1 < self.generation_size <= self.__generation_count:
+            print("Couldn't find result in %d generations" % self.__generation_count)
         elif self.__found_match():
             for population in self.population:
                 if population.fitness == self.fitness:
                     best_fit = population.fitness
-                    epoch_num = self.generation_count
+                    epoch_num = self.__generation_count
                     visualization = population.visualization()
 
         return best_fit, epoch_num, visualization
